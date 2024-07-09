f0c23a64332cb05d9bd705841d7e15fb4d1c6a27
Rodrigo <rodrigo.v.araujo.g@gmail.com>
2024-07-09 16:33:40 -0300

    animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Buttons from './components/Buttons';
-import Header from './components/Header';
-import PorcoesQuentes from './components/PorcoesQuentes';
-import Cervejas from './components/Cervejas'
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Buttons from "./components/Buttons";
+import Header from "./components/Header";
+import PorcoesQuentes from "./components/PorcoesQuentes";
+import Cervejas from "./components/Cervejas";
 
 function App() {
-
-
-  
   return (
     <Router>
-    <div className='bg-amber-100 pt-24 min-h-screen'>
-      <Header />
-      <Buttons />
-      <Routes>
-        <Route path='/porcoesquentes' element={<PorcoesQuentes />}></Route>
-        <Route path='/cervejas' element={<Cervejas />}></Route>
-      </Routes>
-    </div>
+      <div className="bg-amber-100 pt-24 min-h-screen">
+        <Header />
+        <Buttons />
+        <Routes>
+          <Route path="/porcoesquentes" element={<PorcoesQuentes />}></Route>
+          <Route path="/cervejas" element={<Cervejas />}></Route>
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,22 +1,143 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Buttons = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <div className=" rounded-3xl text-1xl bg-white text-black grid grid-flow-col auto-cols-max w-5/7 mt-5 mr-2 ml-2 overflow-x-auto overscroll-contain md:text-1xl md:items-center md:justify-center shadow-lg">
-            <Link to='/menuprincipal' className={` text-1xl p-1 m-3 ${location.pathname === '/menuprincipal' ? 'bg-red-500 rounded-3xl text-white' : ''}`}>Menu Principal</Link>
-            <Link to='/bolinhos' className={` text-1xl p-1 m-3 ${location.pathname === '/bolinhos' ? 'bg-red-500 rounded-3xl text-white' : ''}`}>Bolinhos</Link>
-            <Link to='/porcoesquentes' className={` text-1xl p-1 m-3 ${location.pathname === '/porcoesquentes' ? 'bg-red-500 rounded-3xl text-white' : ''}`}>Porções Quentes</Link>
-            <Link to='/porcoesfrias' className={` text-1xl p-1 m-3 ${location.pathname === '/porcoesfrias' ? 'bg-red-500 rounded-3xl text-white' : ''}`}>Porções Frias</Link>
-            <Link to='/drinks' className={` text-1xl p-1 m-3 ${location.pathname === '/drinks' ? 'bg-red-500 rounded-3xl text-white' : ''}`}>Drinks</Link>
-            <Link to='/cervejas' className={` text-1xl p-1 m-3 ${location.pathname === '/cervejas' ? 'bg-red-500 rounded-3xl text-white' : ''}`}>Cervejas</Link>
-            <Link to='/bebidasquentes' className={` text-1xl p-1 m-3 ${location.pathname === '/bebidasquentes' ? 'bg-red-500 rounded-3xl text-white' : ''}`}>Bebidas Quentes</Link>
-            <Link to='/semalcool' className={` text-1xl p-1 m-3 ${location.pathname === '/semalcool' ? 'bg-red-500 rounded-3xl text-white' : ''}`}>Sem Alcool</Link>
-        </div>
-    );
-}
+  return (
+    <div className="rounded-3xl text-1xl bg-white text-black grid grid-flow-col auto-cols-max w-5/7 mt-5 mr-2 ml-2 overflow-x-auto overscroll-contain md:text-1xl md:items-center md:justify-center shadow-lg">
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="p-1 m-3"
+      >
+        <Link
+          to="/menuprincipal"
+          className={`text-1xl p-1 ${
+            location.pathname === "/menuprincipal"
+              ? "bg-red-500 rounded-3xl text-white p-3"
+              : ""
+          }`}
+        >
+          Menu Principal
+        </Link>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="p-1 m-3"
+      >
+        <Link
+          to="/bolinhos"
+          className={`text-1xl p-1 ${
+            location.pathname === "/bolinhos"
+              ? "bg-red-500 rounded-3xl text-white p-3"
+              : ""
+          }`}
+        >
+          Bolinhos
+        </Link>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="p-1 m-3"
+      >
+        <Link
+          to="/porcoesquentes"
+          className={`text-1xl p-1  ${
+            location.pathname === "/porcoesquentes"
+              ? "bg-red-500 rounded-3xl text-white p-3"
+              : ""
+          }`}
+        >
+          Porções Quentes
+        </Link>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="p-1 m-3"
+      >
+        <Link
+          to="/porcoesfrias"
+          className={`text-1xl p-1  ${
+            location.pathname === "/porcoesfrias"
+              ? "bg-red-500 rounded-3xl text-white p-3"
+              : ""
+          }`}
+        >
+          Porções Frias
+        </Link>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="p-1 m-3"
+      >
+        <Link
+          to="/drinks"
+          className={`text-1xl p-1  ${
+            location.pathname === "/drinks"
+              ? "bg-red-500 rounded-3xl text-white p-3"
+              : ""
+          }`}
+        >
+          Drinks
+        </Link>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="p-1 m-3"
+      >
+        <Link
+          to="/cervejas"
+          className={`text-1xl p-1  ${
+            location.pathname === "/cervejas"
+              ? "bg-red-500 rounded-3xl text-white p-3"
+              : ""
+          }`}
+        >
+          Cervejas
+        </Link>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="p-1 m-3"
+      >
+        <Link
+          to="/bebidasquentes"
+          className={`text-1xl p-1  ${
+            location.pathname === "/bebidasquentes"
+              ? "bg-red-500 rounded-3xl text-white p-3"
+              : ""
+          }`}
+        >
+          Bebidas Quentes
+        </Link>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="p-1 m-3"
+      >
+        <Link
+          to="/semalcool"
+          className={`text-1xl p-1 ${
+            location.pathname === "/semalcool"
+              ? "bg-red-500 rounded-3xl text-white p-3"
+              : ""
+          }`}
+        >
+          Sem Alcool
+        </Link>
+      </motion.div>
+    </div>
+  );
+};
 
 export default Buttons;
