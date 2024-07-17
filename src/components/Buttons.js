707fcf95ee5a050d84ff93ccbbffd226bb2e5b16
Rodrigo <rodrigo.v.araujo.g@gmail.com>
2024-07-17 00:43:03 -0300

    css maybechanges

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,7 +7,37 @@ const Buttons = () => {
   const location = useLocation();
 
   return (
-    <div className="rounded-3xl text-1xl bg-white text-black grid grid-flow-col auto-cols-max w-5/7 mt-5 mr-2 ml-2 overflow-x-auto overscroll-contain md:text-1xl md:items-center md:justify-center shadow-lg">
+    <div className="rounded-3xl text-1xl bg-white text-black grid grid-flow-col auto-cols-max w-5/7 mt-5 mr-2 ml-2 overflow-x-scroll scroll-persistent md:text-1xl md:items-center md:justify-center shadow-lg">
+      <span className="absolute right-4 top-38 text-3xl z-40 text-gray-400">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-right-short"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"
+          />
+        </svg>
+      </span>
+      <span className="absolute left-4 top-38 text-3xl z-40 text-gray-400">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-left-short"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"
+          />
+        </svg>
+      </span>
       <motion.div
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.8 }}
--- src/components/PorcoesFrias.js
+++ src/components/PorcoesFrias.js
@@ -1,10 +1,16 @@
 import React from "react";
 import MenuOption from "./MenuOption";
 
+import polvovinagrete from "../assets/porcaofria/polvoavinagrete.jpeg";
+
 const PorcoesFrias = () => {
   return (
     <div className="grid grid-cols-2 ml-4 md:grid-cols-5">
-      <MenuOption nome="Polvo a Vinagrete" preco="R$ ?,00"></MenuOption>
+      <MenuOption
+        nome="Polvo a Vinagrete"
+        preco="R$ ?,00"
+        local={polvovinagrete}
+      ></MenuOption>
       <MenuOption nome="Ovo de Codorna" preco="R$ ?,00"></MenuOption>
       <MenuOption nome="Azeitona" preco="R$ ?,00"></MenuOption>
       <MenuOption nome="Batata de Festa" preco="R$ ?,00"></MenuOption>.
